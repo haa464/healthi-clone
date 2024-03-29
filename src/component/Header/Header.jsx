53b0f96151ee52e56d439cbb3a207e6f752c93e6
hardik <62943407+haa464@users.noreply.github.com>
2024-03-29 19:05:11 +0530

    Update Header.jsx
    header file change has per mobile view

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './header.css'
 import Logo from '../../assets/color_logo.svg'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Header = () => {
+    const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
     <div className='hreader_cnt'>
         <div className="header_design">
@@ -23,6 +26,20 @@ const Header = () => {
                     <a href="/" className="right_btn">Sign Up</a>
                 </ul>
 
+                <div className="app__navbar-smallscreen">
+                    <GiHamburgerMenu color="#000000" fontSize={27} onClick={() => setToggleMenu(true)} />
+                    {toggleMenu && (
+                    <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+                        <ul className="app__navbar-smallscreen_links">
+                        <li><a href="#home" onClick={() => setToggleMenu(false)}>App</a></li>
+                        <li><a href="#about" onClick={() => setToggleMenu(false)}>Community</a></li>
+                        <li><a href="#menu" onClick={() => setToggleMenu(false)}>shop</a></li>
+                        
+                        </ul>
+                    </div>
+                    )}
+                </div>
                     
             </div>
         </div>
